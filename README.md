0caa9d2eda02f78e027ec5f88e5e2f62f52a3e56
Dan Poggi <dan.poggi@gmail.com>
2017-03-31 00:39:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# jdkswitch
+
+## About
+
+Switches JDKs on macOS.
+
+## Usage
+
+```bash
+npm install -g jdkswitch
+
+jdkswitch
+```
+
+## Screenshot
+
+![jdkswitch Screenshot](/screenshot.png?raw=true)
+
+## License
+
+MIT license, see LICENSE for details.
---
+++ bin/cli.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const table = require("text-table");
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const JDK = require("../lib/jdk");
+
+const jdks = [new JDK(null)].concat(JDK.all);
+
+const currentJdkIndex = jdks.findIndex(jdk => jdk.selected);
+
+const tableRows = table(
+  jdks.map(jdk => jdk.tableRow),
+  {
+    hsep: "      ",
+    stringLength: s => chalk.stripColor(s).length,
+  }
+).split("\n").map((row, i) => {
+  return {
+    name: row,
+    value: i,
+    short: jdks[i].dirName,
+  };
+});
+
+inquirer.prompt([
+  {
+    type: "list",
+    name: "jdk",
+    message: "Which JDK would you like to use?",
+    choices: tableRows,
+    default: currentJdkIndex,
+  },
+]).then(answers => {
+  const jdk = jdks[answers["jdk"]];
+
+  jdk.select();
+
+  console.log(`\n${jdk.selectedMessage}`);
+}).catch(err => {
+  console.log(`\n${err}`);
+
+  process.exit(1);
+});
